ee45d72d1513b6577cd68cbaac5a014e7e05b0c4
SJLee-83 <greatsjlee@gmail.com>
2026-06-17 15:12:40 +0900

    0617_2

--- ch10/Q3_2.py
+++ ch10/Q3_2.py
@@ -36,8 +36,76 @@ df = pd.read_csv("data/heating.csv")
 # print(df.info())
 
 # 1 모든 독립변수를 포함한 회귀모형을 적합. 절편을 제외한 회귀계수의 합 구하기
+from statsmodels.formula.api import ols
 
+model = ols("heating_load ~ wall + roof + glazing + height", data=df).fit()
+# print(model.summary())
+#                             OLS Regression Results                            
+# ==============================================================================
+# Dep. Variable:           heating_load   R-squared:                       0.754
+# Model:                            OLS   Adj. R-squared:                  0.752
+# Method:                 Least Squares   F-statistic:                     417.8
+# Date:                Wed, 17 Jun 2026   Prob (F-statistic):          2.02e-164
+# Time:                        14:45:29   Log-Likelihood:                -1772.0
+# No. Observations:                 550   AIC:                             3554.
+# Df Residuals:                     545   BIC:                             3576.
+# Df Model:                           4                                         
+# Covariance Type:            nonrobust                                         
+# ==============================================================================
+#                  coef    std err          t      P>|t|      [0.025      0.975]
+# ------------------------------------------------------------------------------
+# Intercept     38.3821      1.504     25.517      0.000      35.427      41.337
+# wall           0.0304      0.030      1.012      0.312      -0.029       0.090
+# roof           0.2483      0.006     39.564      0.000       0.236       0.261
+# glazing        0.2217      0.023      9.693      0.000       0.177       0.267
+# height        -0.2469      0.077     -3.212      0.001      -0.398      -0.096
+# ==============================================================================
+# Omnibus:                        0.698   Durbin-Watson:                   1.896
+# Prob(Omnibus):                  0.705   Jarque-Bera (JB):                0.785
+# Skew:                          -0.077   Prob(JB):                        0.675
+# Kurtosis:                       2.899   Cond. No.                         805.
+# ==============================================================================
+
+# Notes:
+# [1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
+# print(round(model.params[1:].sum(), 3)) # 0.254
 
 # 2 유의한 변수(0.05 이하)만을 사용하여 다중회귀분석 다시 수행. R^2 값 구하기
+model = ols("heating_load ~ roof + glazing + height", data=df).fit()
+# print(model.summary())
+#                             OLS Regression Results                            
+# ==============================================================================
+# Dep. Variable:           heating_load   R-squared:                       0.754
+# Model:                            OLS   Adj. R-squared:                  0.752
+# Method:                 Least Squares   F-statistic:                     556.7
+# Date:                Wed, 17 Jun 2026   Prob (F-statistic):          1.31e-165
+# Time:                        15:07:36   Log-Likelihood:                -1772.5
+# No. Observations:                 550   AIC:                             3553.
+# Df Residuals:                     546   BIC:                             3570.
+# Df Model:                           3                                         
+# Covariance Type:            nonrobust                                         
+# ==============================================================================
+#                  coef    std err          t      P>|t|      [0.025      0.975]
+# ------------------------------------------------------------------------------
+# Intercept     39.2337      1.247     31.467      0.000      36.784      41.683
+# roof           0.2480      0.006     39.559      0.000       0.236       0.260
+# glazing        0.2210      0.023      9.668      0.000       0.176       0.266
+# height        -0.2500      0.077     -3.255      0.001      -0.401      -0.099
+# ==============================================================================
+# Omnibus:                        0.592   Durbin-Watson:                   1.896
+# Prob(Omnibus):                  0.744   Jarque-Bera (JB):                0.661
+# Skew:                          -0.075   Prob(JB):                        0.718
+# Kurtosis:                       2.920   Cond. No.                         658.
+# ==============================================================================
+
+# Notes:
+# [1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
+# 0.754
 
 # 3 새로운 건물(wall=20, roof=150, glazing=20, height=5)
+new = pd.DataFrame({"wall":[20], "roof":[150], "glazing":[20], "height":[5]})
+pred = model.predict(new)
+print(round(pred, 3))
+# 0    79.612
+# dtype: float64
+# 79.612
